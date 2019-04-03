db8b60893e97db7651246d22a5a0c8b400e92df8
Jaiden Escudo de roble <jailander49@hotmail.com>
2019-04-02 19:42:48 -0500

    ::IntroducionPHP:: PSR7 es un estandar que nos permite normalizar un request y un response en PHP.

--- public/index.php
+++ public/index.php
@@ -27,17 +27,14 @@ $capsule->setAsGlobal();
 // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
 $capsule->bootEloquent();
 
-$route = $_GET['route'] ?? '/';
-switch ($route) {
-    case '/':
-        require '../index.php';
-        break;
-    case 'addJob':
-        require '../addJob.php';
-        break;
-    case 'addProject':
-        require '../addProject.php';
-        break;
-}
+$request = Zend\Diactoros\ServerRequestFactory::fromGlobals(
+    $_SERVER,
+    $_GET,
+    $_POST,
+    $_COOKIE,
+    $_FILES
+);
+
+var_dump($request->getUri()->getPath());
 
 ?>
