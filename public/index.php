628c7df711baf59bb17f1163e87edaac9f39230a
Jaiden Escudo de roble <jailander49@hotmail.com>
2019-04-02 20:02:16 -0500

    ::IntroduccionPHP:: Router

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ error_reporting(E_ALL);
 require_once '../vendor/autoload.php';
 
 use Illuminate\Database\Capsule\Manager as Capsule;
+use Aura\Router\RouterContainer;
 
 $capsule = new Capsule;
 
@@ -35,6 +36,19 @@ $request = Zend\Diactoros\ServerRequestFactory::fromGlobals(
     $_FILES
 );
 
-var_dump($request->getUri()->getPath());
+$routerContainer = new RouterContainer();
+$map = $routerContainer->getMap();
+
+$map->get('index', '/', '../index.php');
+$map->get('addJobs', '/jobs/add', '../addJob.php');
+$map->get('addProject', '/project/add', '../addProject.php');
+
+$matcher = $routerContainer->getMatcher();
+$route = $matcher->match($request);
+if(!$route){
+    echo 'No Route';
+} else {
+    require $route->handler;
+}
 
 ?>
