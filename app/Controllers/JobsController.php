de7fd96c9b2c3831e5b817f1c41643bd1a8aae9b
Jaiden Escudo de roble <jailander49@hotmail.com>
2019-04-07 01:35:31 -0500

    :.IntroducciónPHP:: Validaciones

--- app/Controllers/JobsController.php
+++ app/Controllers/JobsController.php
@@ -3,22 +3,38 @@
 namespace App\Controllers;
 
 use App\Models\Job;
+use Respect\Validation\Validator as validar;
 
 class JobsController extends BaseController {
 
     public function getAddjobAction($request) {
+        $reponseMessage = '';
+
         if($request->getMethod() == 'POST') {
             $postData = $request->getParsedBody();
+            $jobValidator = validar::key('title', validar::stringType()->notEmpty())
+                    ->key('description', validar::stringType()->notEmpty());
 
-            $job = new Job();
-            $job->title = $postData['title'];
-            $job->description = $postData['description'];
-            $job->save();
+            try {
+                //$jobValidator->validate($postData);// Es true o false
+                $jobValidator->assert($postData);
+
+                $job = new Job();
+                $job->title = $postData['title'];
+                $job->description = $postData['description'];
+                $job->save();
+
+                $reponseMessage = 'Saved';
+            } catch(\Exception $e) {
+                $reponseMessage = $e->getMessage();
+            }
         }
         //var_dump($request->getMethod());
         //var_dump((string)$request->getBody());
         //var_dump($request->getParsedBody());
 
-        return $this->renderHTML('addJob.twig');
+        return $this->renderHTML('addJob.twig', [
+            'reponseMessage' => $reponseMessage
+        ]);
     }
 }
--- app/Controllers/ProjectsController.php
+++ app/Controllers/ProjectsController.php
@@ -3,19 +3,35 @@
 namespace App\Controllers;
 
 use App\Models\Project;
+use Respect\Validation\Validator as validar;
 
 class ProjectsController extends BaseController {
 
-    public function getAddProjectAction($request) {
+    public function getAddProjectAction($request) {    
+        $reponseMessage = '';
+
         if($request->getMethod() == 'POST') {
             $postData = $request->getParsedBody();
+            $jobValidator = validar::key('title', validar::stringType()->notEmpty())
+                    ->key('description', validar::stringType()->notEmpty());
 
-            $project = new Project();
-            $project->title = $postData['title'];
-            $project->description = $postData['description'];
-            $project->save();
+            try {
+                //$jobValidator->validate($postData);// Es true o false
+                $jobValidator->assert($postData);
+
+                $project = new Project();
+                $project->title = $postData['title'];
+                $project->description = $postData['description'];
+                $project->save();
+
+                $reponseMessage = 'Saved';
+            } catch(\Exception $e) {
+                $reponseMessage = $e->getMessage();
+            }
         }
 
-        return $this->renderHTML('addProject.twig');
+        return $this->renderHTML('addProject.twig', [
+            'reponseMessage' => $reponseMessage
+        ]);
     }
 }
